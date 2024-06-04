cae5454890d0d43c923c13b0b5b2501739b3c4a2
Askar.A <aalyiev7@gmail.com>
2024-06-04 14:19:42 +0500

    Initial commit

---
+++ src/main/java/kz/sw_sku_service/SwSkuServiceApplication.java
@@ -0,0 +1,13 @@
+package kz.sw_sku_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwSkuServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwSkuServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/sw_sku_service/SwSkuServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.sw_sku_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwSkuServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
